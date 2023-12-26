f98ddf2fe201f92065c986d594057de6708987c2
Menatallah93 <menatallah9320@gmail.com>
2023-12-26 21:43:59 +0200

    add enum to OtherRequests

--- OnlineCours/Enums.cs
+++ OnlineCours/Enums.cs
@@ -1,16 +1,22 @@
 ﻿namespace OnlineCours
 {
-   
-        public enum StatusOfStudent
-        {
-            Pendding,
-            Accepted,
-            Rejected
-        }
+    public enum StatusOfStudent
+    {
+        Pendding,
+        Accepted,
+        Rejected
+    }
     public enum Grade
     {
-        All=1,
-        Secondry=2
+        All = 1,
+        Secondry = 2
+    }
+
+    public enum OtherRequests
+    {
+        Courses,
+        Consulting,
+        University,
     }
 
 
