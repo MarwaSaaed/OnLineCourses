b21fe5779dea09df82eb5c80ab63f9441ef4e93a
NourElmokhtar <nourelmokhtar87@gmail.com>
2024-01-13 18:54:52 +0200

    GetTutorialForStudent

--- OnlineCours/DTO/UploadTutorialModel.cs
+++ OnlineCours/DTO/UploadTutorialModel.cs
@@ -16,4 +16,20 @@ namespace OnlineCours.DTO
         public string subjectTutorial { get; set; }
         public TutorialType TutorialType { get; set; }
     }
+
+    public class TutorialResponseDto
+    {
+        public string SubjectName { get; set; }
+        public string InstructorName { get; set; }
+        public string Tutorial { get; set; }
+        public TutorialType TutorialType { get; set; }
+
+    }
+    public class TutorialResponse2Dto
+    {
+        public string SubjectName { get; set; }
+        public string InstructorName { get; set; }
+        public List<TutorialData> TutorialDatas { get; set; }
+
+    }
 }
--- OnlineCours/Repository/IstudentRepository.cs
+++ OnlineCours/Repository/IstudentRepository.cs
@@ -7,5 +7,9 @@ namespace OnlineCours.Repository
     public interface IstudentRepository :IPersonRepository<Student> 
     {
         Task<StudentDTO> GetStudentsubject(string studentId, string include);
+        Task<List<TutorialResponseDto>> GetTutorial(string studentId);
+        Task<List<TutorialResponse2Dto>> GetTutorial2(string studentId);
     }
+   
+
 }
